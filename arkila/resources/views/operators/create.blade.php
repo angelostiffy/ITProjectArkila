c49f060b4d5840d67b08669a613c5fdc8c3cbdeb
Angelo Austin Araos <2151848@slu.edu.ph>
2018-02-11 18:46:20 -0800

    Modified OperatorsController.php, finished the basic CRUD for the Operator. Also modified some views.

--- arkila/resources/views/operators/create.blade.php
+++ arkila/resources/views/operators/create.blade.php
@@ -2,44 +2,44 @@
 
 @section('content')
 <h1>Add New Driver </h1><hr>
-<form method="post" action="{{ route('drivers.index')}}">
+<form method="post" action="{{ route('operators.index')}}">
 {{ csrf_field() }}
 
     <div class="form-group">
-        <label >Last Name</label>
-        <input type="text" class="form-control" id="lastName" placeholder="Last Name">
-    </div>
-    <div class="form-group">
         <label >First Name</label>
-        <input type="text" class="form-control" id="firstName" placeholder="First Name">
+        <input type="text" class="form-control" name="firstName" placeholder="First Name">
     </div>
     <div class="form-group">
         <label>Middle Name</label>
-        <input type="text" class="form-control" id="middleName" placeholder="Middle Name">
+        <input type="text" class="form-control" name="middleName" placeholder="Middle Name">
+    </div>
+    <div class="form-group">
+        <label >Last Name</label>
+        <input type="text" class="form-control" name="lastName" placeholder="Last Name">
     </div>
     <div class="form-group">
         <label>Contact Number</label>
-        <input type="text" class="form-control" id="contactNumber" placeholder="Contact Number">
+        <input type="text" class="form-control" name="contactNumber" placeholder="Contact Number">
     </div>
     <div class="form-group">
         <label >Address</label>
-        <input type="text" class="form-control" id="address" placeholder="Address">
+        <input type="text" class="form-control" name="address" placeholder="Address">
     </div>
     <div class="form-group">
         <label >Provincial Address</label>
-        <input type="text" class="form-control" id="provincialAddress" placeholder="Provincial Address">
+        <input type="text" class="form-control" name="provincialAddress" placeholder="Provincial Address">
     </div>
     <div class="form-group">
         <label >Age</label>
-        <input type="number" class="form-control" id="age" placeholder="Age">
+        <input type="number" class="form-control" name="age" placeholder="Age">
     </div>
     <div class="form-group">
         <label >Birth Date</label>
-        <input type="date" class="form-control" id="birthDate" placeholder="Birth Date">
+        <input type="date" class="form-control" name="birthDate" placeholder="Birth Date">
     </div>
     <div class="form-group">
         <label >Birth Place</label>
-        <input type="text" class="form-control" id="birthPlace" placeholder="Birth Place">
+        <input type="text" class="form-control" name="birthPlace" placeholder="Birth Place">
     </div>
     <div class="form-group">
         <label >Gender</label><br>
@@ -48,53 +48,57 @@
     </div>
     <div class="form-group">
         <label >Citizenship</label>
-        <input type="text" class="form-control" id="birthPlace" placeholder="Birth Place">
+        <input type="text" class="form-control" name="citizenship" placeholder="Birth Place">
     </div>
     <div class="form-group">
-        <label >Gender</label><br>
-        <input type="radio" name="cstatus" value="Single">Single<br>
-        <input type="radio" name="cstatus" value="Married">Married<br>
-        <input type="radio" name="cstatus" value="Divorced">Divorced
+        <label >Civil Status</label><br>
+        <input type="radio" name="cStatus" value="Single">Single<br>
+        <input type="radio" name="cStatus" value="Married">Married<br>
+        <input type="radio" name="cStatus" value="Divorced">Divorced
+    </div>
+    <div class="form-group">
+        <label >Number of Childer</label>
+        <input type="text" class="form-control" name="noChild" placeholder="Number of Children">
     </div>
     <div class="form-group">
         <label >Spouse</label>
-        <input type="text" class="form-control" id="spouse" placeholder="Name of Spouse">
+        <input type="text" class="form-control" name="spouse" placeholder="Name of Spouse">
     </div>
     <div class="form-group">
         <label >Spouse Birth Date</label>
-        <input type="date" class="form-control" id="spouseBirthDate" placeholder="Spouse Birth Date">
+        <input type="date" class="form-control" name="spouseBirthDate" placeholder="Spouse Birth Date">
     </div>
     <div class="form-group">
         <label >Father's Name</label>
-        <input type="text" class="form-control" id="fatherName" placeholder="Father's Name">
+        <input type="text" class="form-control" name="father" placeholder="Father's Name">
     </div>
     <div class="form-group">
         <label >Father's Occupation</label>
-        <input type="text" class="form-control" id="fatherOccupation" placeholder="Father's Occupation">
+        <input type="text" class="form-control" name="fatherOccupation" placeholder="Father's Occupation">
     </div>
     <div class="form-group">
         <label >Mother's Name</label>
-        <input type="text" class="form-control" id="motherName" placeholder="Mother's Name">
+        <input type="text" class="form-control" name="mother" placeholder="Mother's Name">
     </div>
     <div class="form-group">
         <label >Mother's Occupation</label>
-        <input type="text" class="form-control" id="motherOccupation" placeholder="Mother's Name">
+        <input type="text" class="form-control" name="motherOccupation" placeholder="Mother's Name">
     </div>
     <div class="form-group">
         <label >Person in Case of Emergency</label>
-        <input type="text" class="form-control" id="pCaseofEmergency" placeholder="Person in Case of Emergency">
+        <input type="text" class="form-control" name="pCaseofEmergency" placeholder="Person in Case of Emergency">
     </div>
     <div class="form-group">
         <label>Emergency Address</label>
-        <input type="text" class="form-control" id="emergencyAddress" placeholder="Emergency Address">
+        <input type="text" class="form-control" name="emergencyAddress" placeholder="Emergency Address">
     </div>
     <div class="form-group">
         <label>Emergency Contact Number</label>
-        <input type="text" class="form-control" id="emergencyContactNo" placeholder="Emergency Contact Number">
+        <input type="text" class="form-control" name="emergencyContactNo" placeholder="Emergency Contact Number">
     </div>
     <div class="form-group">
         <label>SSS ID</label>
-        <input type="tel" pattern="\d*" class="form-control" id="sssId" placeholder="SSS ID">
+        <input type="tel" class="form-control" name="sssId" placeholder="SSS ID">
     </div>
 
 
