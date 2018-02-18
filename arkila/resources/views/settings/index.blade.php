75f8d1d376265415696be56fcebd325c6e9dd332
Angelo Austin Araos <2151848@slu.edu.ph>
2018-02-18 14:58:59 -0800

    Modified web.php. Added index.blade.php and RevenueController.php
    Added index.blade.php for the Settings. Added a route for the newly added controller RevenueController.php

---
+++ arkila/resources/views/settings/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+@section('content')
+	<div class="col-xl-6">
+		<div class="nav-tabs-custom">
+			<ul class="nav nav-tabs">
+				<li class="active"><a href="#tab_1" data-toggle="tab">Destinations</a></li>
+             	<li><a href="#tab_2" data-toggle="tab">Discounts</a></li>
+             	<li><a href="#tab_3" data-toggle="tab">Revenues</a></li>
+			</ul>
+			<div class="tab-content">
+            	<div class="tab-pane active" id="tab_1">
+					
+            	</div>
+            	<div class="tab-pane active" id="tab_2">
+            		
+            	</div>
+            	<div class="tab-pane active" id="tab_3">
+            		<table class="table table-striped table-bordered table-list">
+						<thead>
+							<tr>Revenue</tr>
+							<tr>Amount</tr>
+							<tr>Actions</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td></td>
+							</tr>
+						</tbody>
+            		</table>	
+            	</div>
+            </div>	
+		</div>
+	</div>
+@endsection
