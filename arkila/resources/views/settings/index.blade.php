40657e44e689d3f157c4b8eb46ae254fdcfa058c
Angelo Austin Araos <2151848@slu.edu.ph>
2018-02-19 13:48:45 -0800

    Modified index.blade.php
    added modal boxes for the UI

--- arkila/resources/views/settings/index.blade.php
+++ arkila/resources/views/settings/index.blade.php
@@ -25,19 +25,93 @@
 								<td>{{ $destination->terminal }}</td>
 								<td>{{ $destination->amount }}</td>
 								<td>
-									<a href="/home/settings/{{ $destination->destination_id }}">Edit</a>
+									<button data-toggle="modal" data-target="#modalEditDestination"> Edit </button>
+									<div class="modal fade" id="modalEditDestination" tabindex="-1" role="dialog" 
+								     aria-labelledby="myModalLabel" aria-hidden="true">
+								    <div class="modal-dialog">
+								        <div class="modal-content">
+								            <!-- Modal Header -->
+								            <div class="modal-header">
+								                <button type="button" class="close" 
+								                   data-dismiss="modal">
+								                       <span aria-hidden="true">&times;</span>
+								                       <span class="sr-only">Close</span>
+								                </button>
+								                <h4 class="modal-title" id="myModalLabel">
+								                    Modal title
+								                </h4>
+								            </div>
+								            
+								            <!-- Modal Body -->
+								            <div class="modal-body">
+								                
+								                <form class="form-horizontal" role="form">
+								                  <div class="form-group">
+								                    <label  class="col-sm-2 control-label"
+								                              for="inputEmail3">Email</label>
+								                    <div class="col-sm-10">
+								                        <input type="email" class="form-control" 
+								                        id="inputEmail3" placeholder="Email"/>
+								                    </div>
+								                  </div>
+								                  <div class="form-group">
+								                    <label class="col-sm-2 control-label"
+								                          for="inputPassword3" >Password</label>
+								                    <div class="col-sm-10">
+								                        <input type="password" class="form-control"
+								                            id="inputPassword3" placeholder="Password"/>
+								                    </div>
+								                  </div>
+								                  <div class="form-group">
+								                    <div class="col-sm-offset-2 col-sm-10">
+								                      <div class="checkbox">
+								                        <label>
+								                            <input type="checkbox"/> Remember me
+								                        </label>
+								                      </div>
+								                    </div>
+								                  </div>
+								                  <div class="form-group">
+								                    <div class="col-sm-offset-2 col-sm-10">
+								                      <button type="submit" class="btn btn-default">Sign in</button>
+								                    </div>
+								                  </div>
+								                </form>
+								                
+								                
+								                
+								                
+								                
+								                
+								            </div>
+								            
+								            <!-- Modal Footer -->
+								            <div class="modal-footer">
+								                <button type="button" class="btn btn-default"
+								                        data-dismiss="modal">
+								                            Close
+								                </button>
+								                <button type="button" class="btn btn-primary">
+								                    Save changes
+								                </button>
+								            </div>
+								        </div>
+								    </div>
+								</div>
+									<!-- <a href="/home/settings/{{ $destination->destination_id }}">Edit</a>
                                 		<form action="{{ route('settings.destroy', [$destination->destination_id]) }}" method="POST">
                                  			{{ csrf_field() }}
                                  			<input type="hidden" name="_method" value="DELETE">
                                  			<button>Delete</button>\
-                                 		</form>
+                                 		</form> -->
 								</td>
 							</tr>
 							@endforeach
 						</tbody>
             		</table>
             	</div>
-
+			</div>
+			<div class="tab-content">
             	<div class="tab-pane active" id="tab_2">
 					<table class="table table-striped table-bordered table-list">
 						<thead>
@@ -64,7 +138,8 @@
 						</tbody>
 					</table>
             	</div>
-
+			</div>
+			<div class="tab-content">
             	<div class="tab-pane active" id="tab_3">
                     <table class="table table-striped table-bordered table-list">
                         <thead>
