c346ddc2924a93c4dfae5ce32840ec93a308cea0
randallcaballar <2151211@slu.edu.ph>
2018-02-19 14:46:21 +0800

    added announcements.blade.php

---
+++ arkila/resources/views/announcements/announcements.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.master')
+@section('title', 'index')
+@section('content')
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Announcements 
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Dashboard</li>
+      </ol>
+    </section>
+    <!-- Main content -->
+    <section class="content">
+
+
+          <div class="box col-sm-10" style="padding-top: 1%;">      
+            <div class="box-body">
+                <div class="box box-warning">
+                <div class="box-header with-border bg-yellow">
+                    <h4>Announcement 1</h4>
+                </div>
+                    <div class="box-body"> 
+                        <h6>Created: MM/DD/YYY</h6>
+                        <p>L"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                    </div>           
+                </div>
+              </div>
+            <div class="box-body">
+                <div class="box box-warning">
+                <div class="box-header with-border bg-yellow">
+                    <h4>Announcement 2</h4>
+                </div>
+                    <div class="box-body"> 
+                        <h6>Created: MM/DD/YYY</h6>
+                        <p>L"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                    </div>           
+                </div>
+              </div>
+              <div class="box-body">
+                <div class="box box-warning">
+                <div class="box-header with-border bg-yellow">
+                    <h4>Announcement 3</h4>
+                </div>
+                    <div class="box-body"> 
+                        <h6>Created: MM/DD/YYY</h6>
+                        <p>L"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                    </div>           
+                </div>
+              </div>
+            <!-- /.box-body -->
+          </div>
+    </section>
+    <!-- /.content -->
+  </div>
+@endsection
