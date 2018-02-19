efca0898c35f05f2d5eeed438596392f74e75da9
randallcaballar <2151211@slu.edu.ph>
2018-02-19 18:22:33 +0800

    added blade.php files for operator UI

---
+++ arkila/resources/views/operators/operatorProfile.blade.php
@@ -0,0 +1,211 @@
+@extends('layouts.master')
+@section('title', 'index')
+@section('links')
+@parent
+  <!-- DataTables -->
+  <link rel="stylesheet" href="bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+  <!-- additional CSS -->
+  <link rel="stylesheet" href="operatorStyle.css"> 
+
+@stop
+@section('content')  
+     <section class="content">
+      <div class="row">
+        <div class="col-md-3">
+
+          <!-- Profile Image -->
+          <div class="box box-primary">
+            <div class="box-body box-profile">
+              <img class="profile-user-img img-responsive img-circle" src="dist/img/user4-128x128.jpg" alt="User profile picture">
+
+              <h3 class="profile-username text-center">Shaina Caballar</h3>
+
+              <p class="text-muted text-center">ganda</p>
+                              
+                
+                <ul class="list-group list-group-unbordered">
+                <li class="list-group-item">
+                  <b>ID</b> <a class="pull-right">215</a>
+                </li>
+                <li class="list-group-item">
+                  <b>Contact Number</b> <a class="pull-right">1334287</a>
+                </li>
+              </ul>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+
+          <!-- About Me Box -->
+          <div class="box box-primary">
+            <div class="box-header with-border">
+                <a href="editOperator.html" class="btn btn-block btn-info"><b>Edit Information</b></a>
+                <a href="viewOperator.html" class="btn btn-primary btn-block"><b>View All Information</b></a>
+        <div class="modal fade" id="modal-default">
+          <div class="modal-dialog" style="width:400px;">
+            <div class="modal-content">
+              <div class="modal-header bg-primary" >
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Shaina Caballar</h4>
+              </div>
+              <div class="modal-body">
+                  <div class="container-fluid">
+          <div class="box box-default">
+        <div class="box-header with-border">
+          <h3 class="box-title">Choose Driver</h3>
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body center-block">
+              <div class="form-group ">
+                <select class="form-control">
+                  <option selected="selected">Shaina</option>
+                  <option>Marie</option>
+                  <option>Retuya</option>
+                  <option>Ganda</option>
+                </select>
+              </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.box-body -->
+        <div class="box-footer">
+                <button type="submit" name="search" id="search-btn" class="btn btn-primary pull-right"> Submit </button>
+        </div>
+      </div>
+                  </div>       
+                </div>
+                
+            </div>
+            <!-- /.modal-content -->
+          </div>
+          <!-- /.modal-dialog -->
+        </div>
+                
+            </div>
+            <!-- /.box-header -->
+
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-9">
+          <div class="nav-tabs-custom">
+            <ul class="nav nav-tabs">
+              <li class="active"><a href="#drivers" data-toggle="tab">Drivers</a></li>
+              <li><a href="#vans" data-toggle="tab">Vans</a></li>
+            </ul>
+            <div class="tab-content">
+        <div class="active tab-pane" id="drivers">
+          <div class="box">
+            <!-- /.box-header -->
+            <div class="box-body">
+                <button class="btn btn-success" style="margin-bottom:-5%;"><i class="fa fa-plus-circle"></i> Add Driver</button>
+              <table id="driver" class="table table-bordered table-striped">
+               
+                <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Age</th>
+                  <th>Contact Number</th>
+                  <th>Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <td>puggggg</td>
+                  <td>Chabal loves shaina</td>
+                  <td>15</td>
+                  <td>
+                    <div class="text-center">
+                        <a href="viewDriverOperator.html" class="btn btn-primary"><i class="fa fa-eye"></i>View</a>
+                        <a href="editDriverOperator.html" class="btn btn-info"><i class="fa fa-pencil-square-o"></i>Edit</a>
+                        <button class="btn btn-danger"><i class="fa fa-trash"></i> Delete</i></button>
+                    </div>
+                  </td>
+                </tr>
+                </tbody>
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+              </div>
+              <!-- /.tab-pane -->
+              <div class="tab-pane" id="vans">
+          <div class="box">
+            <div class="box-header">
+                <button class="btn btn-success" style="margin-bottom:-8%;"><i class="fa fa-plus-circle"></i> Add Van</button>
+            </div>
+            <div class="box-body">
+              <table id="van" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                  <th>Plate Number</th>
+                  <th>Driver</th>
+                  <th>Contact Number</th>
+                  <th>Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <th>pug</th>
+                  <td>Chabal loves shaina</td>
+                    <td>0998273</td>
+                  <td>
+                    <div class="text-center">
+                        <a href="changeDriver.html" class="btn btn-info" data-toggle="modal" data-target="#modal-default"><i class="fa fa-eye"></i> Change Driver</a>
+                        <a href="viewVanOperator.html" class="btn btn-primary"><i class="fa fa-pencil-square-o"></i>View</a>
+                        <button class="btn btn-danger"><i class="fa fa-trash"></i> Delete</i></button>
+                    </div>
+                  </td>
+                </tr>
+                </tbody>
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          </div>
+              </div>
+              <!-- /.tab-pane -->
+
+            </div>
+            <!-- /.tab-content -->
+          </div>
+          <!-- /.nav-tabs-custom -->
+        </div>
+        <!-- /.col -->
+      </div>
+
+
+    </section>
+
+@endsection
+
+
+@scripts
+@parent
+    <!-- DataTables -->
+    <script src="bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+    <script src="bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+    <script>
+      $(function () {
+        $('#driver').DataTable({
+          'paging'      : false,
+          'lengthChange': true,
+          'searching'   : true,
+          'ordering'    : true,
+          'info'        : true,
+          'autoWidth'   : true
+        })
+        $('#van').DataTable({
+          'paging'      : false,
+          'lengthChange': true,
+          'searching'   : true,
+          'ordering'    : true,
+          'info'        : true,
+          'autoWidth'   : true
+        })
+      })
+    </script>
+    
+@stop
---
+++ arkila/resources/views/operators/viewDriverOperator.blade.php
@@ -0,0 +1,196 @@
+@extends('layouts.master')
+@section('title', 'index')
+@section('links')
+@parent
+  <!-- DataTables -->
+  <link rel="stylesheet" href="bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+  <!-- additional CSS -->
+  <link rel="stylesheet" href="operatorStyle.css"> 
+
+@stop
+@section('content')                 
+                 <div class="box ">
+                 <div class="box box-header">
+                     <h3 style="margin-right: 10px; display: inline-block;"><strong>Edit Driver Information</strong></h3><button class="btn btn-info" onclick="grayer('f_1',false);" style="float: right; margin-top: 2%;"><i class="fa fa-pencil-square-o"></i> Edit</button>
+                 </div>
+                <form id="f_1" action="#" onsubmit="return false;" class="form-horizontal">
+                 <div class="box-body">
+                    <div class="col-md-6">
+                        <div  class="form-group">
+                            <label for="driverId" class="control-label col-sm-4">Driver ID</label>
+                            <div class="col-sm-8">
+                            <input type="text" id="driverId" name="driverId" class="form-control">
+                            </div>
+                        </div>
+                        <div  class="form-group">
+                            <label for="operatorId" class="control-label col-sm-4">Operator ID</label>
+                            <div class="col-sm-8">
+                            <input type="text" id="operatorId" name="operatorId" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="contactNumberD" class="control-label col-sm-4">Contact number</label>
+                            <div class="col-sm-8">
+                            <input type="text" id="contactNumberD" name="contactNumberD" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="addressD" class="control-label col-sm-4">Address</label>
+                            <div class="col-sm-8">
+                            <input type="text" id="addressD" name="addressD" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="provincialAddressD" class="control-label col-sm-4">Provincial address</label>
+                            <div class="col-sm-8">
+                            <input type="text" id="provincialAddressD" name="provincialAddressD" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="ageD" class="control-label col-sm-4">Age</label>
+                            <div class="col-sm-8">
+                            <input type="text" id="ageD" name="ageD" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="birthDateD" class="control-label col-sm-4">Date of birth</label>
+                            <div class="col-sm-8">
+                            <input type="text" id="birthDateD" name="birthDateD" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="birthPlaceD" class="control-label col-sm-4">Place of birth</label>
+                            <div class="col-sm-8">
+                            <input type="text" id="birthPlaceD" name="birthPlaceD" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="personCasedOfEmergencyD" class="control-label col-sm-4">Contact person</label>
+                            <div class="col-sm-8">
+                            <input type="text" id="personCasedOfEmergencyD" name="personCasedOfEmergencyD" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="emergencyContactD" class="control-label col-sm-4">Emergency contact no.</label>
+                            <div class="col-sm-8">
+                            <input type="text" id="emergencyContactD" name="emergencyContactD" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="emergencyAddressD" class="control-label col-sm-4">Emergency address</label>
+                            <div class="col-sm-8">
+                            <input type="text" id="emergencyAddressD" name="emergencyAddressD" class="form-control">
+                            </div>
+                        </div>
+                      </div>
+                      
+                      
+                      <div class="col-md-6">
+                        <div  class="form-group">
+                            <label for="genderD" class="control-label col-sm-4">Gender</label>
+                            <div class="col-sm-8">
+                            <input type="text" id="genderD" name="genderD" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="citizenshipD" class="control-label col-sm-4">Citizenship</label>
+                            <div class="col-sm-8">
+                            <input type="text" id="citizenshipD" name="citizenshipD" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="civilStatusD" class="control-label col-sm-4">Civil status</label>
+                            <div class="col-sm-8">
+                            <input type="text" id="civilStatusD" name="civilStatusD" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="numberOfChildrenD" class="control-label col-sm-4">Number of children</label>
+                            <div class="col-sm-8">
+                            <input type="text" id="numberOfChildrenD" name="numberOfChildrenD" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="spouseD" class="control-label col-sm-4">Spouse</label>
+                            <div class="col-sm-8">
+                            <input type="text" id="spouseD" name="spouseD" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="spouseBirthdateD" class="control-label col-sm-4">Spouse birthdate</label>
+                            <div class="col-sm-8">
+                            <input type="text" id="spouseBirthdateD" name="spouseBirthdateD" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="fatherNameD" class="control-label col-sm-4">Name of Father</label>
+                            <div class="col-sm-8">
+                            <input type="text" id="fatherNameD" name="fatherNameD" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="fatherOccupationD" class="control-label col-sm-4">Occupation of Father</label>
+                            <div class="col-sm-8">
+                            <input type="text" id="fatherOccupationD" name="fatherOccupationD" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="motherNameD" class="control-label col-sm-4">Name of Mother</label>
+                            <div class="col-sm-8">
+                            <input type="text" id="motherNameD" name="motherNameD" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="motherOccupationD" class="control-label col-sm-4">Occupation of Mother</label>
+                            <div class="col-sm-8">
+                            <input type="text" id="motherOccupationD" name="motherOccupationD" class="form-control">
+                            </div>
+                        </div>   
+                        <div class="form-group">
+                            <label for="sssD" class="control-label col-sm-4">SSS number</label>
+                            <div class="col-sm-8">
+                            <input type="text" id="sssD" name="sssD" class="form-control">
+                            </div>
+                        </div>   
+                      </div>
+                        </div>
+                    </div>
+                     </form>
+                    <div class="box-footer">
+                <button class="btn btn-primary pull-right" onclick="grayer('f_1',true);">Save changes</button>
+                    </div>
+        </div>
+        </div>
+
+@endsection
+
+@scripts
+@parent
+    <!-- DataTables -->
+    <script src="bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+    <script src="bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+    <script>
+      $(function () {
+        $('#driver').DataTable()
+        $('#van').DataTable({
+          'paging'      : true,
+          'lengthChange': true,
+          'searching'   : true,
+          'ordering'    : true,
+          'info'        : true,
+          'autoWidth'   : true
+        })
+      })
+    </script>
+    <script type="text/javascript">
+    function grayer(formId, yesNo) {
+       var f = document.getElementById(formId), s, opacity;
+       s = f.style;
+       s.opacity = s.MozOpacity = s.KhtmlOpacity = opacity/100;
+       s.filter = 'alpha(opacity='+opacity+')';
+       for(var i=0; i<f.length; i++) f[i].disabled = yesNo;
+    }
+    window.onload=function(){grayer('f_1',true);}; // disabled by default
+    </script>
+    
+@stop
