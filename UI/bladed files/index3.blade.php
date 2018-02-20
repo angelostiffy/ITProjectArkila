a551dbfe7e4e0735084f94547c991f2be8f728da
teodoro delson <teodelson23@gmail.com>
2018-02-20 13:38:38 +0800

    Added bladed html files to be deployed

---
+++ UI/bladed files/index3.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.master')
+@section('title', 'index')
+@section('links')
+@parent
+  <link rel="stylesheet" href="public\css\myOwnStyle.css">
+  @stop
+@section('content')
+
+        <div class="box box-primary"  >
+            <div class="box-body"  >
+                <h3>Van Information</h3>
+              <div class="row">
+                <div class="col-md-5">
+                  Plate Number:<input type="text" class="form-control fixVanForm" placeholder="Last Name">
+                
+             
+              <div id="buttonsCenter">
+                   <button type="submit" class="btn btn-primary">Clear</button>
+                  <button type="submit" class="btn btn-success">Add Van</button>
+              
+                </div>
+              
+                
+                
+              </div>
+            </div>
+            <!-- /.box-body -->
+          </div>
+
+
+          </div>
+          
+         
+
+@endsection
+
+@section('scripts')
+@parent
+
+@endsection
