4656df1f174851bd7400778b75223baaeb7ea310
Angelo Austin Araos <2151848@slu.edu.ph>
2018-02-19 18:10:42 -0800

    Modified the logic for the destination CRUD

--- arkila/routes/web.php
+++ arkila/routes/web.php
@@ -25,10 +25,13 @@ Route::resource('home/announcements', 'AnnouncementsController');
 Route::resource('home/drivers', 'DriversController');
 Route::resource('home/operators', 'OperatorsController');
 Route::resource('home/vans', 'VansController');
+Route::resource('home/settings/destinations', 'DestinationController', [
+	'except' => ['index','create','show', 'edit']
+]);
 Route::resource('home/settings/fees', 'FeesController', [
     'except' => ['index','show']
 ]);
 Route::resource('home/settings/discounts', 'DiscountsController', [
     'except' => ['index','show']
 ]);
-Route::get('home/settings/', 'HomeController@settings');
+Route::get('home/settings', 'HomeController@settings');
