31573eb5887730b125638e55e22d9cef7d3db0af
Juan Miguel Delos Santos <2153820@slu.edu.ph>
2018-02-26 16:41:35 +0800

    added usermanagement.blade in usermanagemet dir

---
+++ arkila/resources/views/usermanagement/userManagement.blade.php
@@ -0,0 +1,156 @@
+@extends('layouts.master')
+@section('title', 'User Management')
+@section('links')
+@parent
+  <!-- DataTables -->
+  <link rel="stylesheet" href="{{ URL::asset('adminlte/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css') }}">
+  <!-- additional CSS -->
+  <link rel="stylesheet" href="tripModal.css"> 
+
+@stop
+@section('content')
+
+<section class="content">
+    <div class="box">
+        
+        <div class="box-body">
+        <div class="col-xl-6">
+        <!-- Custom Tabs -->
+        <div class="nav-tabs-custom">
+            <ul class="nav nav-tabs">
+<li class="active"><a href="#tab_1" data-toggle="tab">Admin</a></li>
+<li><a href="#tab_2" data-toggle="tab">Driver</a></li>
+<li><a href="#tab_3" data-toggle="tab">Customer</a></li>
+
+</ul>
+
+            <div class="tab-content">
+            <div class="tab-pane active" id="tab_1">
+<div class="box-body">
+
+<div class="center-block">
+<button class="btn btn-primary"><i class="glyphicon glyphicon-plus">Add </i></button>
+
+</div>    
+
+<table id="adminTable" class="table table-bordered table-striped">
+<thead>
+<tr>
+<th>Name</th>
+<th>Username</th>    
+<th>Terminal</th>
+<th>Action</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+
+<td></td>  
+<td></td>      
+<td></td>
+<td class="center-block">
+<div class="center-block">
+     <button class="btn btn-default"><i class="glyphicon glyphicon-cog"></i></button>
+
+</div>
+</td>
+
+</table>
+</div>
+
+
+</div>
+
+            <!-- /.tab-pane -->
+            <div class="tab-pane" id="tab_2">
+
+            <div class="box-body">
+
+                <table id="driverTable" class="table table-bordered table-striped">
+                    <thead>
+<tr>
+
+<th>Name</th>
+<th>Username</th>     
+<th>Action</th>
+</tr>
+</thead>
+                    <tbody>
+                    <tr>
+
+                    <td></td>
+                    <td></td>
+                    
+                    <td class="center-block">
+                    <div class="center-block">
+                        <button class="btn btn-default"><i class="glyphicon glyphicon-cog">View</i></button>
+
+                    </div>
+                    </td>
+
+                </table>
+            </div>
+            </div>
+
+
+            <div class="tab-pane" id="tab_3">
+            <div class="box-body">   
+            <table id="customerTable" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+
+                <th>Name</th>
+                <th>Username</th>
+                <th>Email Address</th>    
+                <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+
+                <td></td>
+                <td></td>
+                <td></td>
+                <td class="center-block">
+                    <div class="center-block">
+                         <button class="btn btn-default"><i class="glyphicon glyphicon-cog"></i></button>
+
+                    </div>
+                </td>
+                </table>
+                </div>
+                </div>
+
+
+                <!-- /.box-body -->
+                </div>
+                </div>
+                <!-- /.tab-pane -->
+                </div>
+            <!-- /.tab-content -->
+            </div>
+</div>
+</section>
+@endsection
+
+@section('scripts')
+@parent
+    <!-- DataTables -->
+    <script src="{{ URL::asset('adminlte/bower_components/datatables.net/js/jquery.dataTables.min.js') }}")></script>
+    <script src="{{ URL::asset('adminlte/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js') }}"></script>
+            
+<script>
+  $(function () {
+    $('#example2').DataTable()
+    $('#dailyLedgerTable').DataTable({
+      'paging'      : true,
+      'lengthChange': true,
+      'searching'   : true,
+      'ordering'    : true,
+      'info'        : false,
+      'autoWidth'   : true
+    })
+  })
+</script>
+
+@endsection
