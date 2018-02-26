dac850da498a0955a966fa70255736d268ea7df3
Juan Miguel Delos Santos <2153820@slu.edu.ph>
2018-02-26 14:36:36 +0800

    added semi colon in ine 22 of user table migration

--- arkila/database/migrations/2014_10_12_000000_create_users_table.php
+++ arkila/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('username');
             $table->string('email')->unique()->nullable();
             $table->string('password');
-            $table->enum('user_type' ['Driver', 'Customer', 'Admin', 'Super-Admin']);
+            $table->enum('user_type' , ['Driver', 'Customer', 'Admin', 'Super-Admin']);
             $table->enum('status' ['enable', 'disable'])
             $table->enum('user_type' ,['Driver', 'Customer', 'Admin', 'Super-Admin']);
             $table->rememberToken();
