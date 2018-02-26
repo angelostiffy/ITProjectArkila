c6e55299d66ee6e88b00c74633a2f3c849700af2
Angelo Austin Araos <2151848@slu.edu.ph>
2018-02-26 13:24:22 -0800

    Added a yield for the method_field in blade.phps of form and form_lg

--- arkila/resources/views/layouts/form_lg.blade.php
+++ arkila/resources/views/layouts/form_lg.blade.php
@@ -57,6 +57,7 @@
                 <!-- /.content -->
                 <form id="@yield('form-id')" action="@yield('form-action')" method="POST">
                 {{csrf_field()}}
+                @yield('method_field')
                 <section class="content">
                     
                 @yield('form-body')
--- arkila/resources/views/settings/editTerminal.blade.php
+++ arkila/resources/views/settings/editTerminal.blade.php
@@ -1,12 +1,13 @@
 @extends('layouts.form')
 @section('title', 'Edit Terminal')
 @section('back-link')
-@section('form-action', route('terminal.update', [$terminal->id]))
+@section('form-action', route('terminal.update', [$terminal->terminal_id]))
+@section('method_field', method_field('PATCH'))
 @section('form-title', 'Edit Terminal')
 @section('form-body')
 
 	@include('message.error')
-	{{method_field('PATCH')}}
+	
     <div class="form-group">
         <label>Terminal Name:</label>
         <input type="text" class="form-control" name="editTerminalName" value="{{$terminal->terminals}}">
