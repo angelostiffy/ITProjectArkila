6fff958ec2a95453dcc4e4913582973af758070c
Juan Miguel Delos Santos <2153820@slu.edu.ph>
2018-02-27 20:05:37 +0800

    fixed

--- arkila/resources/views/usermanagement/editAdmin.blade.php
+++ arkila/resources/views/usermanagement/editAdmin.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.form')
-@section('title', 'Add Daily Revenue/Expense')
+@section('title', 'Manange Users')
 @section('form-title', 'Manage Account')
 @section('links')
 @parent
@@ -8,23 +8,26 @@
 @section('content')
 @section('form-body')
           
-                    <div class="form-group">
-                      <label for="payor">User name:</label>
-                      <span>Span</span>
-                    </div>
-                    <div class="form-group">
-                      <label for="Particulars">Name:</label>
-                      <span>Yuki Marfil</span>
-                    </div>
-                    <div class="form-group">
-                      <label for="Particulars">Email Address:</label>
-                      <span>yuki@grkngc.com</span>
-                    </div>
+  <div class="form-group">
+    <label for="payor">User name:</label>
+    <span>Span</span>
+  </div>
+  <div class="form-group">
+    <label for="Particulars">Name:</label>
+    <span>Yuki Marfil</span>
+  </div>
+  <div class="form-group">
+    <label for="Particulars">Email Address:</label>
+    <span>yuki@grkngc.com</span>
+  </div>
+
+  <div>
+  <button type="button" class="btn btn-danger" data-dismiss="modal">Reset Password</button> </div>                 
 
                    
 @endsection
 @section('form-btn')     
-   <button type="button" class="btn btn-success" data-dismiss="modal">Save Changes</button>
+   <button type="button" class="btn btn-primary" data-dismiss="modal">Save Changes</button>
    <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
 
 @endsection
