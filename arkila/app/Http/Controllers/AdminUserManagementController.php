135e3fa698ac85050daea3a13eb972f0f2771bfb
Angelo Austin Araos <2151848@slu.edu.ph>
2018-02-28 07:53:41 -0800

    Modified the AdminUserManagementController

--- arkila/app/Http/Controllers/AdminUserManagementController.php
+++ arkila/app/Http/Controllers/AdminUserManagementController.php
@@ -24,13 +24,10 @@ class AdminUserManagementController extends Controller
     public function store()
     {
         $this->validate(request(), [
-<<<<<<< HEAD
-            "userName" => "required|max:15",
+
             "fullName" => "required|max:50",
-=======
-            "fullName" => "required|max:50",
-            "userName" => "unique:users,username|required|max:15",
->>>>>>> a95726966a3252db6a07f6309e4c6a0a8f4566bd
+            "userName" => ['required',new checkUserName, 'max:15'],
+
             "userEmail" => "email",
             "password" => "required|confirmed",
             "addUserTerminal" => ['required', new checkTerminal, 'max:40']
