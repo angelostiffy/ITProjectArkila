353c6d1f7344a46fdd1e727928dfb9daa1287500
Juan Miguel Delos Santos <2153820@slu.edu.ph>
2018-03-19 00:22:21 +0800

    added asterisk in settings

--- arkila/resources/views/settings/editDiscount.blade.php
+++ arkila/resources/views/settings/editDiscount.blade.php
@@ -7,40 +7,16 @@
 @section('form-body')
 	
 	<div>
-	 	<label for="description">Description:</label>
+	 	<label for="description">Description: <span class="text-red">*</span></label>
 	 	<p>{{$discount->description}}</p>
 	 </div>
 
     <div class="form-group">
-        <label>Amount:</label>
+        <label>Amount: <span class="text-red">*</span></label>
         <input type="number" class="form-control" name="editDiscountAmount" step = "0.25" min="1"  max="5000" value="{{$discount->amount}}" required>
     </div>
 
 @endsection
 @section('form-btn')
-    <a type="button" class="btn btn-primary" data-toggle="modal" data-target="#form-modal" data-keyboard="true">Save Changes</a>
-@endsection
-
-@section('modal-title','Confirm')
-@section('modal-body')
-    <p>Are you sure you want to overwrite the changes?</p>
-@endsection
-
-@section('modal-btn')
-    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-    <button type="submit" class="btn btn-primary" style="width:33%;">Submit</button>
-@endsection
-
-@section('scripts') 
-@parent
-    <script>
-        $(document).keypress(function(event){
-            var keycode = (event.keyCode ? event.keyCode : event.which);
-            if(keycode == '13'){
-                $('#form-modal').modal('toggle');
-                event.preventDefault();
-            }
-        });   
-    </script>
-
+    <button type="submit" class="btn btn-primary">Save Changes</button>
 @endsection
