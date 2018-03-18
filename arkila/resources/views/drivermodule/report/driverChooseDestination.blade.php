f96151f1d70670b58c27b50c4a0926b523a6f36b
randallcaballar <2151211@slu.edu.ph>
2018-03-18 21:25:26 +0800

    added choose destination for reports in driver module

---
+++ arkila/resources/views/drivermodule/report/driverChooseDestination.blade.php
@@ -0,0 +1,28 @@
+ @extends('layouts.driver') 
+ @section('title', 'Driver Report') 
+ @section('content-title', 'Driver Report') 
+ @section('content')
+ 
+<div class="row">
+    <div class="col-md-3"></div>
+    <div class="col-md-6">
+        <div class="box">
+            <div class="box-header">
+                <h3>Choose Terminal</h3>
+            </div>
+            <div class="box-body">
+                <div class="form-group">
+                    <div class="list-group">
+                        <a href="#" class="list-group-item list-group-item-action">Cabanatuan</a>
+                        <a href="#" class="list-group-item list-group-item-action">San Jose</a>
+                    </div>
+                </div>
+            </div>
+            <div class="box-footer">
+                <button class="btn btn-primary btn-group-justified">Select Terminal</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
