bda5bdd03d0eb36a244f4b1fd000b3d339971052
Juan Miguel Delos Santos <2153820@slu.edu.ph>
2018-03-22 14:47:59 +0800

    fixed the format of error page

--- arkila/resources/views/errors/500.blade.php
+++ arkila/resources/views/errors/500.blade.php
@@ -1,18 +1,18 @@
-@extends('layouts.master') 
+@extends('layouts.error') 
 @section('title', 'Error 505') 
 @section('content') 
 
-<div class="error-page">
-    <h2 class="headline text-red">500</h2>
+<div class="error-page" style="padding:20%">
 
-    <div class="error-content">
+    <div class="section text-center" style="margin-top:14%">       
+        <h1 class="text-red" style="font-size:200pt"> 500</h1>       
         <h3><i class="fa fa-warning text-red"></i> Oops! Something went wrong.</h3>
-
         <p>
-            We will work on fixing that right away. Meanwhile, you may <a href="/home">return to dashboard</a> 
+            We will work on fixing that right away.
         </p>
 
     </div>
+    
 </div>
 <!-- /.error-page -->
 
