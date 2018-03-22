3c2967b9d37abe3b6c68070313e6ede5f8f4ffb9
Juan Miguel Delos Santos <2153820@slu.edu.ph>
2018-03-22 15:52:16 +0800

    added ther error page

---
+++ arkila/resources/views/errors/409.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.error') 
+@section('title', 'Error 404') 
+@section('content') 
+
+<div class="error-page">
+
+    <div class="section text-center" style="margin-top:14%">       
+        <h1 class="text-yellow" style="font-size:200pt"> 409</h1>       
+        <h3><i class="fa fa-warning text-yellow"></i> Oops! Page not found.</h3>
+        <p>
+            We could not find the page you were looking for.
+        </p>
+
+    </div>
+    <!-- /.error-content -->
+</div>
+
+@endsection
