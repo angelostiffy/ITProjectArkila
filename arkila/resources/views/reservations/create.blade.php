8e4d49cfc1c680790ee3689217e633745eb3c1c4
randallcaballar <2151211@slu.edu.ph>
2018-03-26 04:14:06 +0800

    edited button classes for admin module (rental and reservation)

--- arkila/resources/views/reservations/create.blade.php
+++ arkila/resources/views/reservations/create.blade.php
@@ -146,8 +146,8 @@
 <div class="box box-footer">
     <div style="overflow:auto;">
         <div style="float:right;">
-            <button type="button" id="prevBtn" onclick="nextPrev(-1)" class="btn btn-default">Previous</button>
-            <button type="button" id="nextBtn" onclick="nextPrev(1); getData();" class="btn btn-primary">Next</button>
+            <button type="button" id="prevBtn" onclick="nextPrev(-1)" class="btn btn-default btn-sm">Previous</button>
+            <button type="button" id="nextBtn" onclick="nextPrev(1); getData();" class="btn btn-primary btn-sm">Next</button>
         </div>
     </div>
 </div>
