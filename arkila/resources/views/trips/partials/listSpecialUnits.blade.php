a95eb65b4c02ad4e1223d656c8f6344300786300
Dixon Aldwin Viloria <2152015@slu.edu.ph>
2018-03-25 19:34:37 +0800

    changed design of queue

--- arkila/resources/views/trips/partials/listSpecialUnits.blade.php
+++ arkila/resources/views/trips/partials/listSpecialUnits.blade.php
@@ -1,19 +1,27 @@
 @foreach($trips as $trip)
-    <li class="list-group-item">
+    <li class="">
+        <span class="list-border">
+            <div class="row">
+        <div class="col-xs-6">
         <h4 class="pull-left">
             {{$trip->plate_number}}
-            <span class="badge badge-pill badge-default ">{{$trip->remarks}}</span>
+            
         </h4>
-        <div class="btn-group pull-right">
-            <button type="button" class="btn btn-default btn-sm btn-flat dropdown-toggle" data-toggle="dropdown">
-                <span class="caret"></span>
-                <span class="sr-only">Toggle Dropdown</span>
-            </button>
-            <ul class="dropdown-menu" role="menu">
-                <li><a href="#">On Deck</a></li>
-                <li><a href="#">Remove</a></li>
-            </ul>
+
         </div>
-        <div class="clearfix"></div>
+        <div class="col-xs-6">
+            <div class="pull-right">
+                <i class="badge badge-pill badge-default ">{{$trip->remarks}}</i>
+                <button type="button" class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown" style="border-radius: 100%;">
+                    <i class="fa fa-gear"></i>
+                </button>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">On Deck</a></li>
+                    <li><a href="#">Remove</a></li>
+                </ul>
+            </div>
+        </div>
+        </div>
+        </span>
     </li>
     @endforeach
