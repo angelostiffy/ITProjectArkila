50632509c9a254102836315c6d0090ad93a89883
Angelo Austin Araos <2151848@slu.edu.ph>
2018-03-28 23:44:24 +0800

    Changed the login view made by Randall Dog maliit titi

--- arkila/resources/views/login/login.blade.php
+++ arkila/resources/views/login/login.blade.php
@@ -1,54 +1,60 @@
-@include('layouts.partials.stylesheets')
-<style>
-    body,
-    html {
-        height: 100%;
-        background-repeat: no-repeat;
-        background-image: linear-gradient(rgb(104, 145, 162), rgb(12, 97, 33));
-    }
-
-    .card-container.card {
-        max-width: 350px;
-        padding: 40px 40px;
-    }
-
-
-    .card {
-        background-color: #F7F7F7;
-        padding: 20px 25px 30px;
-        margin: 0 auto 25px;
-        margin-top: 50px;
-    }
-
-    #userImg {
-        margin-bottom: 20px;
-    }
-
-    #imgSize {
-        width: 200px;
-        height: 200px;
-
-    }
-</style>
-
-<div class="container">
-    <div class="card card-container">
-        <div id="userImg" class="text-center">
-            <img id="imgSize" src="{{ URL::asset('img/user_icon.png') }}">
-        </div>
-        <form method="POST" action="/login">
-          {{csrf_field()}}
-            <input type="text" name="username" class="form-control" style="margin-bottom:3%" placeholder="Username" maxlength="20" required autofocus>
-            <input type="password" name="password" class="form-control" placeholder="Password" style="margin-bottom:8%" maxlength="20" required>
-            <div class="checkbox">
-                
+@extends('layouts.customer_non_user')
+@section('content')
+<section id="mainSection" style="background-image: url('{{ URL::asset('img/background.jpg') }}');">
+        <div class="container">
+            <div class="heading text-center">
+                <h2>Sign In</h2>
             </div>
-            <button class="btn btn btn-primary btn-block" type="submit">Log in</button>
-        </form>
-        <!-- /form -->
-        <a href="#" class="forgot-password pull-right">Forgot Password?</a>
-    </div>
-    <!-- /card-container -->
-</div>
-<!-- /container -->
-@include('layouts.partials.scripts')
+            <div class="row">
+                <div class="col-md-4"></div>
+                <div class="col-md-4" id="boxContainer">
+                    <form class="contact100-form" method="POST" action="{{ route('login') }}">
+                      {{csrf_field()}}
+                        <div class="wrap-input100{{ $errors->has('username') ? ' has-error' : '' }}">
+                            <input id="username" type="text" name="username" class="input100" type="text" name="Customer Username" placeholder="Username">
+                            @if ($errors->has('username'))
+                              <span class="focus-input100">
+                                <strong>{{ $errors->first('email') }}</strong>
+                              </span>
+                            @endif
+                        </div><!-- wrap-input100-->
+                        <div class="wrap-input100{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <input id="password" type="password" name="password" class="input100" type="password" name="Customer Password" placeholder="Password">
+                            @if ($errors->has('password'))
+                              <span class="focus-input100">
+                                <strong>{{ $errors->first('password') }}</strong>
+                              </span>
+                            @endif
+                        </div><!-- wrap-input100-->
+                        <div class="checkbox">
+                          <label>
+                              <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                          </label>
+                        </div>
+                        <div class="container-contact100-form-btn">
+                          <div class="row">
+                            <div class="col-md-2">
+                                <button type="submit" class="contact100-form-btn"><strong>Log In</strong></button>
+                            </div>
+                            <div class="col-md-2">
+                                <button type="submit" class="contact100-form-btn"><strong>Log In</strong></button>
+                            </div>
+                          </div>
+
+
+                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                Forgot Your Password?
+                            </a>
+                        </div><!-- container-contact100-form-btn-->
+                    </form>
+                    <!-- contact100-form-->
+                </div>
+                <!-- boxContainer-->
+            </div>
+            <!-- row-->
+        </div>
+        <!-- container-->
+    </section>
+    <!--    main section-->
+
+@stop
--- arkila/routes/web.php
+++ arkila/routes/web.php
@@ -207,8 +207,7 @@ Route::get('/', function () {
 
 
 /*************************************Driver Module****************************/
-
-/********************Dashboard************************/
+/******************************************************************************/
 Route::group(['middleware' => ['auth', 'driver']], function(){
   /*Driver Dashboard*/
   Route::get('/home/driver-dashboard', 'DriverModuleControllers\DriverHomeController@index')->name('drivermodule.index');
@@ -230,10 +229,17 @@ Route::group(['middleware' => ['auth', 'driver']], function(){
   Route::get('/home/view-trips', 'DriverModuleControllers\TripLogController@viewTripLog')->name('drivermodule.triplog.driverTripLog');
 
 });
+/******************************************************************************/
+/******************************************************************************/
+Route::get('/home/try', 'PassController@index');
 
+/*********************************Customer Module******************************/
+/******************************************************************************/
+Route::get('/home', 'CustomerModuleControllers\CustomerHomeController@indexNonUser')->name('customermodule.non-user.index');
+Route::get('/about', 'CustomerModuleControllers\CustomerHomeController@aboutNonUser')->name('customermodule.non-user.about.customerAbout');
 
-    Route::get('/home/try', 'PassController@index');
-//Route::get('home/profile', 'DriverModuleControllers\DriverProfileController@index');
-/******************************************************/
+Route::group(['middleware' => ['auth', 'customer']], function(){
 
+});
+/******************************************************************************/
 /******************************************************************************/
