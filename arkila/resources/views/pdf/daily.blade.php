b63ddae5ad397e2c2835f88e872c5be271ee76b0
Marvie Yuki <marvieyuki06@gmail.com>
2018-03-31 23:11:28 +0800

    change title for ledger pdf

--- arkila/resources/views/pdf/daily.blade.php
+++ arkila/resources/views/pdf/daily.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Drivers</title>
+    <title>Daily Ledger</title>
 </head>
 <body>
 <style>
