c176cf45d12d34a2e8233745ab431528ecfca1ff
Marvie Yuki <marvieyuki06@gmail.com>
2018-04-01 02:48:30 +0800

    general ledger printing via pdf

--- arkila/resources/views/ledger/generalLedger.blade.php
+++ arkila/resources/views/ledger/generalLedger.blade.php
@@ -143,17 +143,25 @@
 <script src="https://cdn.datatables.net/buttons/1.5.1/js/dataTables.buttons.min.js"> </script>
 <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.flash.min.js"> </script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"> </script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"> </script>
+<script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.html5.min.js"> </script>
+<script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.print.min.js"> </script>
+
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"> </script>
 
 <script>
+
     $(document).ready(function() {
         $('.generalLedgerTable').DataTable({
             dom: 'Bfrtip',
             buttons: [
                 {
-                    extend: 'pdf',
+                    extend: 'pdfHtml5',
+                    text: 'Print',
                     orientation: 'landscape',
-                    pageSize: 'LEGAL'
+                    pageSize: 'LEGAL',
+                    title: 'Ban Trans General Ledger',
                 }
             ],
             'paging': false,
